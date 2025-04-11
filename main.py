8c476049047b03417ef7cca617e625761ed7ffd4
Antrax <fredcox@gmail.com>
2025-04-10 21:19:26 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import docker
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
+
+
+def main():
+    client: docker.DockerClient = docker.from_env()
+    containers = client.containers.list()
+
+    for container in containers:
+        print(f"ID: {container.id}")
+        print(f"Name: {container.name}")
+        print(
+            f"Image: {container.image.tags[0] if container.image.tags else 'No tags'}"
+        )
+        print(f"Status: {container.status}")
+
+        print(f"Created: {container.attrs['Created']}")
+        print(f"Ports: {container.ports}")
+        print("=" * 40)
+
+
+if __name__ == "__main__":
+    main()
