79c60393ede1d5e12e2174b887815caf6e249df1
Antrax <fredcox@gmail.com>
2025-04-18 15:34:18 -0300

    new name. now app is dockerwatch

--- Dockerfile
+++ Dockerfile
@@ -27,5 +27,5 @@ USER ${USER_NAME}
 RUN uv venv --python 3.13
 
 # Command to run the application
-CMD uv run docker-watch -c /app/config/config.yaml
+CMD uv run dockerwatch -c /app/config/config.yaml
 
--- src/dockerwatch/constants.py
+++ src/dockerwatch/constants.py
@@ -4,7 +4,7 @@ import os
 
 APP_VERSION = "0.1.3"
 # This is the name of the application.
-APP_NAME = "docker-watch"
+APP_NAME = "dockerwatch"
 
 DEFAULT_CONFIG_FILE = os.path.join(
     os.path.expanduser(path="~"), ".config", f"{APP_NAME}", "config.yaml"
